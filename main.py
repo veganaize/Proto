73d2033a6f99a07f4df8363c2cf63afe4a8789f1
veganaiZe <veganaiZe@protonmail.com>
2023-10-25 13:30:24 -0700

    Parse web address from command line

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
+import argparse
 import sys
 
 import Browser
 
 
-def display_page(address='http://example.com'):
-    argv = sys.argv
-    if len(argv) > 1: address = argv[1]
+homepage = 'http://example.com/index.html'
+
+
+def parse_arguments():
+    argument_parser = argparse.ArgumentParser(
+            prog='Proto',
+            description='Web browser written in Python 3.4',
+            epilog='https://github.com/veganaiZe/Proto'
+    )
+    
+    argument_parser.add_argument(
+            'address',
+            default=homepage,
+            help='A web address with an "http://" or "https://" prefix.',
+            nargs='?'
+    )
+    
+    arguments = argument_parser.parse_args()
+
+    print('arguments.address:', arguments.address)
+    return arguments.address
+
+
+def display_page(address=homepage):
     page = Browser.request_url(address)
     page.render()
 
 
-display_page()
+if __name__ == '__main__':
+    address = parse_arguments()
+    display_page(address)
